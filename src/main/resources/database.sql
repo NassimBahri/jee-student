64e8296b55250142edc994c299d727d1d081f9bd
Nassim Bahri <bahri.nassim@gmail.com>
2025-12-08 16:26:26 +0100

    Mise à jour du fichier SQL

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -4,12 +4,13 @@ CREATE TABLE `admin` (
      `nom` varchar(50) NOT NULL,
      `login` varchar(100) NOT NULL,
      `password` varchar(250) NOT NULL,
+     `role` varchar(250) NOT NULL,
      PRIMARY KEY (`id`)
 );
 
 INSERT INTO `admin` (`id`, `nom`, `login`, `password`) VALUES
-    (1,	'Nassim',	'nassim',	'dfa8b570f448cb6949aec9625f85ef110ca320cb');
-
+   (1,	'Nassim',	'nassim',	'dfa8b570f448cb6949aec9625f85ef110ca320cb',	'admin'),
+   (2,	'Manager',	'manager',	'1a8565a9dc72048ba03b4156be3e569f22771f23',	'manager');
 DROP TABLE IF EXISTS `etudiant`;
 CREATE TABLE `etudiant` (
     `id` int NOT NULL AUTO_INCREMENT,
